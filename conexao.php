426fe216b06c848f4da661ad0645ae7f753f7ebc
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-06-14 00:13:05 -0400

    Iniciando a Index e painel

--- conexao.php
+++ conexao.php
@@ -3,6 +3,9 @@ require_once('config.php');
 
 date_default_timezone_set('America/Campo_Grande');
 
+try{
 $pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8","$usuario","$senha");
-
+}catch(Exception $e){
+    echo "Error ao conectar com o banco de dados!"."<br />".$e->getMessage;
+}
 ?>
