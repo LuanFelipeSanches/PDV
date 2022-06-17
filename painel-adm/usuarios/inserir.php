5aa6c1dc8d9681d3cf8e37318a20492de844db0a
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-06-16 22:37:28 -0400

    Tratando duplicidade

--- painel-adm/usuarios/inserir.php
+++ painel-adm/usuarios/inserir.php
@@ -7,12 +7,34 @@ $cpf = $_POST['cpf'];
 $senha = $_POST['senha'];
 $nivel = $_POST['nivel'];
 
-$res = $pdo->prepare("INSERT INTO usuarios 
-SET nome = :nome, email = :email, cpf = :cpf,senha = :senha,  nivel = :nivel");
+//EVITAR DUPLICIDADE NO EMAIL
+$query_con = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+$query_con -> bindValue(":email", $email);
+$query_con -> execute();
+$res_con = $query_con->fetchAll(PDO::FETCH_ASSOC);
+if (@count($res_con) > 0) {
+    echo 'O email do usuário já está cadastrado!';
+    exit();
+}
 
-$res->bindValue(':nome', $nome);
-$res->bindValue(':email', $email);
-$res->bindValue(':cpf', $cpf);
-$res->bindValue(':senha', $senha);
-$res->bindValue(':nivel', $nivel);
-?>
+//EVITAR DUPLICIDADE NO CPF
+$query_con = $pdo->prepare("SELECT * FROM usuarios WHERE cpf = :cpf");
+$query_con -> bindValue(":cpf", $cpf);
+$query_con -> execute();
+$res_con = $query_con->fetchAll(PDO::FETCH_ASSOC);
+if (@count($res_con) > 0) {
+    echo 'O CPF do usuário já está cadastrado!';
+    exit();
+}
+
+$res = $pdo->prepare("INSERT INTO usuarios 
+SET nome = :nome, email = :email, cpf = :cpf, senha = :senha, nivel = :nivel");
+
+$res -> bindValue(':nome', $nome);
+$res -> bindValue(':email', $email);
+$res -> bindValue(':cpf', $cpf);
+$res -> bindValue(':senha', $senha);
+$res -> bindValue(':nivel', $nivel);
+$res -> execute();
+
+echo 'Salvo com Sucesso!';
