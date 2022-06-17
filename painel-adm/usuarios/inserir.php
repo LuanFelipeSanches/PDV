f36816180537a32e8ecd068661d74ff0f4653042
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-06-16 22:05:05 -0400

    Insert

--- painel-adm/usuarios/inserir.php
+++ painel-adm/usuarios/inserir.php
@@ -1,3 +1,18 @@
 <?php
-echo "Salvo com Sucesso";
+require_once("../../conexao.php");
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$cpf = $_POST['cpf'];
+$senha = $_POST['senha'];
+$nivel = $_POST['nivel'];
+
+$res = $pdo->prepare("INSERT INTO usuarios 
+SET nome = :nome, email = :email, cpf = :cpf,senha = :senha,  nivel = :nivel");
+
+$res->bindValue(':nome', $nome);
+$res->bindValue(':email', $email);
+$res->bindValue(':cpf', $cpf);
+$res->bindValue(':senha', $senha);
+$res->bindValue(':nivel', $nivel);
 ?>
