c96bf8d06a5ca9579f6a33ad3eb52c758051e634
LuanFelipeSanches <95869993+LuanFelipeSanches@users.noreply.github.com>
2022-06-24 00:36:47 -0400

    Codigo de barras e Banco de dados no select input

--- painel-adm/produtos.php
+++ painel-adm/produtos.php
@@ -1,21 +1,21 @@
-<?php
+<?php 
 $pag = 'produtos';
 @session_start();
 
 require_once('../conexao.php');
-require_once('verificar-permissao.php')
+require_once('verificar-permissao.php');
 
 ?>
 
-<a href="index.php?pagina=<?php echo $pag ?>&funcao=novo" type="button" class="btn btn-secondary mt-2">Nova Produto</a>
+<a href="index.php?pagina=<?php echo $pag ?>&funcao=novo" type="button" class="btn btn-secondary mt-2">Novo Produto</a>
 
 <div class="mt-4" style="margin-right:25px">
-	<?php
-	$query = $pdo->query("SELECT * FROM produtos ORDER BY id DESC");
+	<?php 
+	$query = $pdo->query("SELECT * from produtos order by id desc");
 	$res = $query->fetchAll(PDO::FETCH_ASSOC);
 	$total_reg = @count($res);
-	if ($total_reg > 0) {
-	?>
+	if($total_reg > 0){ 
+		?>
 		<small>
 			<table id="example" class="table table-hover my-4" style="width:100%">
 				<thead>
@@ -28,45 +28,65 @@ require_once('verificar-permissao.php')
 						<th>Fornecedor</th>
 						<th>Foto</th>
 						<th>Ações</th>
-
+						
 					</tr>
 				</thead>
 				<tbody>
 
-					<?php
-					for ($i = 0; $i < $total_reg; $i++) {
-						foreach ($res[$i] as $key => $value) {
+					<?php 
+					for($i=0; $i < $total_reg; $i++){
+						foreach ($res[$i] as $key => $value){	}
+
+							$id_cat = $res[$i]['categoria'];
+						$query_2 = $pdo->query("SELECT * from categorias where id = '$id_cat'");
+						$res_2 = $query_2->fetchAll(PDO::FETCH_ASSOC);
+						$nome_cat = $res_2[0]['nome'];
+
+
+						//BUSCAR OS DADOS DO FORNECEDOR
+						$id_forn = $res[$i]['fornecedor'];
+						$query_f = $pdo->query("SELECT * from fornecedores where id = '$id_forn'");
+						$res_f = $query_f->fetchAll(PDO::FETCH_ASSOC);
+						$total_reg_f = @count($res_f);
+						if($total_reg_f > 0){ 
+							$nome_forn = $res_f[0]['nome'];
+							$tel_forn = $res_f[0]['telefone'];
+						}else{
+							$nome_forn = '';
+							$tel_forn = '';
 						}
-						$id_cat = $res[$i]['categoria'];
-						$query_cat = $pdo->query("SELECT * FROM categorias WHERE id = $id_cat'");
-						$res_cat = $query_cat->fetchAll(PDO::FETCH_ASSOC);
-						$nome_cat = $res_cat[0]['nome'];
-					?>
+
+						?>
 
 						<tr>
 							<td><?php echo $res[$i]['nome'] ?></td>
 							<td><?php echo $res[$i]['codigo'] ?></td>
 							<td><?php echo $res[$i]['estoque'] ?></td>
-							<td><?php echo $res[$i]['valor_compra'] ?></td>
-							<td><?php echo $res[$i]['valor_venda'] ?></td>
-							<td><?php echo $res[$i]['fornecedor'] ?></td>
-							<td></td>
-
-							<td><img src="../img/categorias/<?php echo $res[$i]['foto'] ?>" width="40"></td>
+							<td>R$ <?php echo number_format($res[$i]['valor_compra'], 2, ',', '.'); ?></td>
+							<td>R$ <?php echo number_format($res[$i]['valor_venda'], 2, ',', '.'); ?></td>
+							<td><?php echo $nome_forn ?></td>
+							
+							<td><img src="../img/<?php echo $pag ?>/<?php echo $res[$i]['foto'] ?>" width="40"></td>
 							<td>
-								<a href="index.php?pagina=<?php echo $pag ?>&funcao=editar&id=<?php echo $res[$i]['id'] ?>" title="Editar Registro">
+								<a href="index.php?pagina=<?php echo $pag ?>&funcao=editar&id=<?php echo $res[$i]['id'] ?>" title="Editar Registro" style="text-decoration: none">
 									<i class="bi bi-pencil-square text-primary"></i>
 								</a>
 
-								<a href="index.php?pagina=<?php echo $pag ?>&funcao=deletar&id=<?php echo $res[$i]['id'] ?>" title="Excluir Registro">
+								<a href="index.php?pagina=<?php echo $pag ?>&funcao=deletar&id=<?php echo $res[$i]['id'] ?>" title="Excluir Registro" style="text-decoration: none">
 									<i class="bi bi-archive text-danger mx-1"></i>
-
-
-									<a href="#" onclick="mostrarDados('<?php echo $res[$i]['descricao'] ?>', 
-								'<?php echo $res[$i]['foto'] ?>','<?php echo $nome_cat ?>')" title="Ver Descrição">
-										<i class="bi bi-card-text text-dark"></i></i>
-									</a>
 								</a>
+
+								<a href="#" onclick="mostrarDados('<?php echo $res[$i]['nome'] ?>', '<?php echo $res[$i]['descricao'] ?>', '<?php echo $res[$i]['foto'] ?>', '<?php echo $nome_cat ?>', '<?php echo $nome_forn ?>', '<?php echo $tel_forn ?>')" title="Ver Descriçao" style="text-decoration: none">
+									<i class="bi bi-card-text text-dark mx-1"></i>
+								</a>
+
+
+								<a href="#" onclick="comprarProdutos('<?php echo $res[$i]['id'] ?>')" title="Comprar Produtos" style="text-decoration: none">
+									<i class="bi bi-bag text-success mx-1"></i>
+								</a>
+
+
+
 							</td>
 						</tr>
 
@@ -76,19 +96,19 @@ require_once('verificar-permissao.php')
 
 			</table>
 		</small>
-	<?php } else {
+	<?php }else{
 		echo '<p>Não existem dados para serem exibidos!!';
 	} ?>
 </div>
 
 
-<?php
-if (@$_GET['funcao'] == "editar") {
+<?php 
+if(@$_GET['funcao'] == "editar"){
 	$titulo_modal = 'Editar Registro';
-	$query = $pdo->query("SELECT * FROM produtos WHERE id = '$_GET[id]'");
+	$query = $pdo->query("SELECT * from produtos where id = '$_GET[id]'");
 	$res = $query->fetchAll(PDO::FETCH_ASSOC);
 	$total_reg = @count($res);
-	if ($total_reg > 0) {
+	if($total_reg > 0){ 
 		$nome = $res[0]['nome'];
 		$codigo = $res[0]['codigo'];
 		$categoria = $res[0]['categoria'];
@@ -98,8 +118,9 @@ if (@$_GET['funcao'] == "editar") {
 		$valor_compra = $res[0]['valor_compra'];
 		$valor_venda = $res[0]['valor_venda'];
 		$foto = $res[0]['foto'];
+
 	}
-} else {
+}else{
 	$titulo_modal = 'Inserir Registro';
 }
 ?>
@@ -222,7 +243,7 @@ if (@$_GET['funcao'] == "editar") {
 
 
 
-<div class="modal fade" tabindex="-1" id="modalDeletar">
+<div class="modal fade" tabindex="-1" id="modalDeletar" >
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -234,11 +255,9 @@ if (@$_GET['funcao'] == "editar") {
 
 					<p>Deseja Realmente Excluir o Registro?</p>
 
-					<small>
-						<div align="center" class="mt-1" id="mensagem-excluir">
-
-						</div>
-					</small>
+					<small><div align="center" class="mt-1" id="mensagem-excluir">
+						
+					</div> </small>
 
 				</div>
 				<div class="modal-footer">
@@ -252,32 +271,136 @@ if (@$_GET['funcao'] == "editar") {
 		</div>
 	</div>
 </div>
-<div class="modal fade" tabindex="-1" id="modalDados">
+
+
+
+
+
+
+<div class="modal fade" tabindex="-1" id="modalDados" >
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Dados do Fornecedor</h5>
+				<h5 class="modal-title"><span id="nome-registro"></span></h5>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
-
+			
 			<div class="modal-body mb-4">
 
 				<b>Categoria: </b>
 				<span id="categoria-registro"></span>
 				<hr>
+				
+				<div id="div-forn">
+					<span class="mr-4">
+						<b>Fornecedor: </b>
+						<span id="nome-forn-registro"></span>
+					</span>
+					
+					<span class="mr-4">
+						<b>Telefone: </b>
+						<span id="tel-forn-registro"></span>
+					</span>
+					<hr>
+				</div>
+
+
+				
 				<b>Descrição: </b>
 				<span id="descricao-registro"></span>
 				<hr>
-				<img id="imagem-registro" src="" class="mt-4" width="300">
-			</div>
+				<img id="imagem-registro" src="" class="mt-4" width="200">
+			</div> 
 
 		</div>
 	</div>
 </div>
 
 
-<?php
-if (@$_GET['funcao'] == "novo") { ?>
+
+
+
+
+<div class="modal fade" tabindex="-1" id="modalComprar" >
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">Fazer Pedido</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<form method="POST" id="form-comprar">
+				<div class="modal-body">
+
+					<div class="mb-3">
+						<label for="exampleFormControlInput1" class="form-label">Fornecedor</label>
+						<select class="form-select mt-1" aria-label="Default select example" name="fornecedor">
+							<?php 
+							$query = $pdo->query("SELECT * from fornecedores order by nome asc");
+							$res = $query->fetchAll(PDO::FETCH_ASSOC);
+							$total_reg = @count($res);
+							if($total_reg > 0){ 
+
+								for($i=0; $i < $total_reg; $i++){
+									foreach ($res[$i] as $key => $value){	}
+										?>
+
+									<option value="<?php echo $res[$i]['id'] ?>"><?php echo $res[$i]['nome'] ?></option>
+
+								<?php }
+
+							}else{ 
+								echo '<option value="">Cadastre um Fornecedor</option>';
+
+							} ?>
+
+
+						</select>
+					</div> 
+
+
+					<div class="row">
+						<div class="col-6">
+							
+							<div class="mb-3">
+								<label for="exampleFormControlInput1" class="form-label">Valor Compra</label>
+								<input type="text" class="form-control" id="valor_compra" name="valor_compra" placeholder="Valor Compra" required="">
+							</div> 
+							
+						</div>
+						<div class="col-6">
+							
+							<div class="mb-3">
+								<label for="exampleFormControlInput1" class="form-label">Quantidade</label>
+								<input type="number" class="form-control" id="quantidade" name="quantidade" placeholder="Quantidade" required="" >
+							</div> 
+							
+						</div>
+					</div>
+
+					<small><div align="center" class="mt-1" id="mensagem-comprar">
+						
+					</div> </small>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" id="btn-fechar-comprar" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+					<button name="btn-salvar-comprar" id="btn-salvar-comprar" type="submit" class="btn btn-success">Salvar</button>
+
+					<input name="id-comprar" id="id-comprar" type="hidden">
+
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<?php 
+if(@$_GET['funcao'] == "novo"){ ?>
 	<script type="text/javascript">
 		var myModal = new bootstrap.Modal(document.getElementById('modalCadastrar'), {
 			backdrop: 'static'
@@ -289,8 +412,8 @@ if (@$_GET['funcao'] == "novo") { ?>
 
 
 
-<?php
-if (@$_GET['funcao'] == "editar") { ?>
+<?php 
+if(@$_GET['funcao'] == "editar"){ ?>
 	<script type="text/javascript">
 		var myModal = new bootstrap.Modal(document.getElementById('modalCadastrar'), {
 			backdrop: 'static'
@@ -302,11 +425,11 @@ if (@$_GET['funcao'] == "editar") { ?>
 
 
 
-<?php
-if (@$_GET['funcao'] == "deletar") { ?>
+<?php 
+if(@$_GET['funcao'] == "deletar"){ ?>
 	<script type="text/javascript">
 		var myModal = new bootstrap.Modal(document.getElementById('modalDeletar'), {
-
+			
 		})
 
 		myModal.show();
@@ -318,8 +441,8 @@ if (@$_GET['funcao'] == "deletar") { ?>
 
 <!--AJAX PARA INSERÇÃO E EDIÇÃO DOS DADOS COM IMAGEM -->
 <script type="text/javascript">
-	$("#form").submit(function() {
-		var pag = "<?= $pag ?>";
+	$("#form").submit(function () {
+		var pag = "<?=$pag?>";
 		event.preventDefault();
 		var formData = new FormData(this);
 
@@ -328,39 +451,39 @@ if (@$_GET['funcao'] == "deletar") { ?>
 			type: 'POST',
 			data: formData,
 
-			success: function(mensagem) {
+			success: function (mensagem) {
 
 				$('#mensagem').removeClass()
 
 				if (mensagem.trim() == "Salvo com Sucesso!") {
 
-					//$('#nome').val('');
-					//$('#cpf').val('');
-					$('#btn-fechar').click();
-					window.location = "index.php?pagina=" + pag;
+                    //$('#nome').val('');
+                    //$('#cpf').val('');
+                    $('#btn-fechar').click();
+                    window.location = "index.php?pagina="+pag;
 
-				} else {
+                } else {
 
-					$('#mensagem').addClass('text-danger')
-				}
+                	$('#mensagem').addClass('text-danger')
+                }
 
-				$('#mensagem').text(mensagem)
+                $('#mensagem').text(mensagem)
 
-			},
+            },
 
-			cache: false,
-			contentType: false,
-			processData: false,
-			xhr: function() { // Custom XMLHttpRequest
-				var myXhr = $.ajaxSettings.xhr();
-				if (myXhr.upload) { // Avalia se tem suporte a propriedade upload
-					myXhr.upload.addEventListener('progress', function() {
-						/* faz alguma coisa durante o progresso do upload */
-					}, false);
-				}
-				return myXhr;
-			}
-		});
+            cache: false,
+            contentType: false,
+            processData: false,
+            xhr: function () {  // Custom XMLHttpRequest
+            	var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) { // Avalia se tem suporte a propriedade upload
+                	myXhr.upload.addEventListener('progress', function () {
+                		/* faz alguma coisa durante o progresso do upload */
+                	}, false);
+                }
+                return myXhr;
+            }
+        });
 	});
 </script>
 
@@ -369,8 +492,8 @@ if (@$_GET['funcao'] == "deletar") { ?>
 
 <!--AJAX PARA EXCLUIR DADOS -->
 <script type="text/javascript">
-	$("#form-excluir").submit(function() {
-		var pag = "<?= $pag ?>";
+	$("#form-excluir").submit(function () {
+		var pag = "<?=$pag?>";
 		event.preventDefault();
 		var formData = new FormData(this);
 
@@ -379,7 +502,7 @@ if (@$_GET['funcao'] == "deletar") { ?>
 			type: 'POST',
 			data: formData,
 
-			success: function(mensagem) {
+			success: function (mensagem) {
 
 				$('#mensagem').removeClass()
 
@@ -388,7 +511,7 @@ if (@$_GET['funcao'] == "deletar") { ?>
 					$('#mensagem-excluir').addClass('text-success')
 
 					$('#btn-fechar').click();
-					window.location = "index.php?pagina=" + pag;
+					window.location = "index.php?pagina="+pag;
 
 				} else {
 
@@ -411,10 +534,11 @@ if (@$_GET['funcao'] == "deletar") { ?>
 
 <script type="text/javascript">
 	$(document).ready(function() {
+		gerarCodigo();
 		$('#example').DataTable({
 			"ordering": false
 		});
-	});
+	} );
 </script>
 
 
@@ -424,13 +548,14 @@ if (@$_GET['funcao'] == "deletar") { ?>
 
 <!--SCRIPT PARA CARREGAR IMAGEM -->
 <script type="text/javascript">
+
 	function carregarImg() {
 
 		var target = document.getElementById('target');
 		var file = document.querySelector("input[type=file]").files[0];
 		var reader = new FileReader();
 
-		reader.onloadend = function() {
+		reader.onloadend = function () {
 			target.src = reader.result;
 		};
 
@@ -442,20 +567,134 @@ if (@$_GET['funcao'] == "deletar") { ?>
 			target.src = "";
 		}
 	}
+
 </script>
 
+
+
+
 <script type="text/javascript">
-	function mostrarDados(descricao, foto, categoria) {
+	function mostrarDados(nome, descricao, foto, categoria, nome_forn, tel_forn){
 		event.preventDefault();
 
-		$('#categoria-registro').text(endereco);
-		$('#descricao-registro').text(nome);
+		if(nome_forn.trim() === ""){
+			document.getElementById("div-forn").style.display = 'none';
+		}else{
+			document.getElementById("div-forn").style.display = 'block';
+		}
+
+		$('#nome-registro').text(nome);
+		$('#categoria-registro').text(categoria);
+		$('#descricao-registro').text(descricao);
+		$('#nome-forn-registro').text(nome_forn);
+		$('#tel-forn-registro').text(tel_forn);
+		
 		$('#imagem-registro').attr('src', '../img/produtos/' + foto);
 
-		var myModal = new bootstrap.Modal(document.getElementById('modalDados'), {
 
+		var myModal = new bootstrap.Modal(document.getElementById('modalDados'), {
+			
 		})
 
 		myModal.show();
 	}
+</script>
+
+
+
+
+
+
+
+<!--AJAX PARA EXCLUIR DADOS -->
+<script type="text/javascript">
+	$("#codigo").keyup(function () {
+		gerarCodigo();
+	});
+</script>
+
+
+<script type="text/javascript">
+	var pag = "<?=$pag?>";
+	function gerarCodigo(){
+		$.ajax({
+			url: pag + "/barras.php",
+			method: 'POST',
+			data: $('#form').serialize(),
+			dataType: "html",
+
+			success:function(result){
+				$("#codigoBarra").html(result);
+			}
+		});
+	}
+</script>
+
+
+
+<script type="text/javascript">
+	function comprarProdutos(id){
+		event.preventDefault();
+
+		$('#id-comprar').val(id);
+
+		var myModal = new bootstrap.Modal(document.getElementById('modalComprar'), {
+			
+		})
+		myModal.show();
+	}
+</script>
+
+
+
+
+
+
+
+<!--AJAX PARA COMPRAR PRODUTO -->
+<script type="text/javascript">
+	$("#form-comprar").submit(function () {
+		var pag = "<?=$pag?>";
+		event.preventDefault();
+		var formData = new FormData(this);
+
+		$.ajax({
+			url: pag + "/comprar-produto.php",
+			type: 'POST',
+			data: formData,
+
+			success: function (mensagem) {
+
+				$('#mensagem-comprar').removeClass()
+
+				if (mensagem.trim() == "Salvo com Sucesso!") {
+
+                    //$('#nome').val('');
+                    //$('#cpf').val('');
+                    $('#btn-fechar').click();
+                    window.location = "index.php?pagina="+pag;
+
+                } else {
+
+                	$('#mensagem-comprar').addClass('text-danger')
+                }
+
+                $('#mensagem-comprar').text(mensagem)
+
+            },
+
+            cache: false,
+            contentType: false,
+            processData: false,
+            xhr: function () {  // Custom XMLHttpRequest
+            	var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) { // Avalia se tem suporte a propriedade upload
+                	myXhr.upload.addEventListener('progress', function () {
+                		/* faz alguma coisa durante o progresso do upload */
+                	}, false);
+                }
+                return myXhr;
+            }
+        });
+	});
 </script>
